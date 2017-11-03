6d30eeca2d74ceea1006ad39560b9078b5d975d3
ITHope <nadya.berdnik@mail.ru>
2017-11-03 14:00:24 +0200

    Add NUnit

---
+++ UnitTestSeleniumCalculator/UnitTestCalcJS/NUnitTestCalcJS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSTest;
+using JSTest.ScriptLibraries;
+
+namespace UnitTestCalcJS
+{
+    [TestClass]
+    public class UnitTestCalcJS
+    {
+        private readonly TestScript _commonTestScript = new TestScript();
+
+        [TestInitialize]
+        public void CommonJavaScriptTests()
+        {
+            _commonTestScript.AppendFile("E:\\CSharpDev\\Projects\\Calculator\\CalculatorJS\\Calculator.js");
+            _commonTestScript.AppendBlock(new JsAssertLibrary());
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestCalcPlus()
+        {
+            _commonTestScript.RunTest(@"assert.equal(4, Calc(2, 2, '+'));");
+        }
+
+        [TestMethod]
+        public void TestCalcMinus()
+        {
+            _commonTestScript.RunTest(@"assert.equal(8, Calc(10, 2, '-'));");
+        }
+
+        [TestMethod]
+        public void TestCalcDiv()
+        {
+            _commonTestScript.RunTest(@"assert.equal(5, Calc(10, 2, '/'));");
+        }
+
+        [TestMethod]
+        public void TestCalcMult()
+        {
+            _commonTestScript.RunTest(@"assert.equal(20, Calc(10, 2, '*'));");
+        }
+    }
+}
--- UnitTestSeleniumCalculator/UnitTestCalcJS/UnitTestCalcJS.cs
+++ UnitTestSeleniumCalculator/UnitTestCalcJS/UnitTestCalcJS.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using JSTest;
 using JSTest.ScriptLibraries;
 
 namespace UnitTestCalcJS
 {
-    [TestClass]
-    public class UnitTestCalcJS
+    [TestFixture]
+    public class NUnitTestCalcJS
     {
         private readonly TestScript _commonTestScript = new TestScript();
 
-        [TestInitialize]
+        [Test]
         public void CommonJavaScriptTests()
         {
             _commonTestScript.AppendFile("E:\\CSharpDev\\Projects\\Calculator\\CalculatorJS\\Calculator.js");
@@ -19,25 +19,25 @@ namespace UnitTestCalcJS
 
         public TestContext TestContext { get; set; }
 
-        [TestMethod]
+        [Test]
         public void TestCalcPlus()
         {
             _commonTestScript.RunTest(@"assert.equal(4, Calc(2, 2, '+'));");
         }
 
-        [TestMethod]
+        [Test]
         public void TestCalcMinus()
         {
             _commonTestScript.RunTest(@"assert.equal(8, Calc(10, 2, '-'));");
         }
 
-        [TestMethod]
+        [Test]
         public void TestCalcDiv()
         {
             _commonTestScript.RunTest(@"assert.equal(5, Calc(10, 2, '/'));");
         }
 
-        [TestMethod]
+        [Test]
         public void TestCalcMult()
         {
             _commonTestScript.RunTest(@"assert.equal(20, Calc(10, 2, '*'));");
